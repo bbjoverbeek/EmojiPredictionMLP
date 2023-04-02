f5abb70bdd3a5fe93ee6534eae55bd0d00e58ded
Oscar <o.zwagers@icloud.com>
2023-04-02 10:18:40 +0200

    made some changes to finetune.py to be equal to the finetune notebook.

--- finetune.py
+++ finetune.py
@@ -1,52 +1,89 @@
-from transformers import (
-    AutoTokenizer,
-    AutoModelForSequenceClassification,
-    TrainingArguments,
-    Trainer
-)
-from torch.utils.data import DataLoader
-from datasets import load_dataset
+from TweetNormalizer import normalizeTweet
+# clone bertweet repo into content folder
 import evaluate
 import numpy as np
+from datasets import load_dataset
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    Trainer,
+    TrainingArguments
+)
+import torch
+from sklearn.metrics import (
+    accuracy_score,
+    recall_score,
+    precision_score,
+    f1_score
+)
 
-
-def tokenize(data):
-    return tokenizer(data["text"], padding="max_length", truncation=True)
-
-
-def compute_metrics(eval_pred):
-    logits, labels = eval_pred
-    predictions = np.argmax(logits, axis=-1)
-    return metric.compute(predictions=predictions, references=labels)
-
-
-seed = 42
-
+evaluate_metric = evaluate.load("accuracy")
+f1_metric = evaluate.load("f1")
+seed = 45
 dataset = load_dataset("tweet_eval", "emoji")
 tokenizer = AutoTokenizer.from_pretrained("vinai/bertweet-base")
 
 
-dataset = dataset.map(tokenize, batched=True)
+def tokenize(data):
+    """
+    Using the normalize tweet function from bertweet, so the text is optimized
+    for the bertweet model.
+    Then the text is tokenized and padded and truncated. The max length is set
+    by the max length of the bertweet model.
+    """
+    return tokenizer(
+        normalizeTweet(data["text"]), padding="max_length", truncation=True
+    )
 
-small_train = dataset["train"].shuffle(seed=seed).select(range(1000))
-small_eval = dataset["validation"].shuffle(seed=seed).select(range(1000))
+
+def compute_metrics(p):
+    """
+    This will be run after each epoch. It will calculate the accuracy, macro
+    recall, macro precision and macro f1-score.
+    """
+    pred, labels = p
+    pred = np.argmax(pred, axis=1)
+
+    accuracy = accuracy_score(y_true=labels, y_pred=pred)
+    recall = recall_score(y_true=labels, y_pred=pred, average="macro")
+    precision = precision_score(y_true=labels, y_pred=pred, average="macro")
+    f1 = f1_score(y_true=labels, y_pred=pred, average='macro')
+
+    return {
+        "accuracy": accuracy,
+        "precision": precision,
+        "recall": recall,
+        "f1": f1
+    }
+
+
+dataset = dataset.map(tokenize)
+train = dataset["train"].shuffle(seed=seed)
+eval = dataset["validation"].shuffle(seed=seed)
+test = dataset["test"].shuffle(seed=seed)
+
+print(train[0:5])
 
 model = AutoModelForSequenceClassification \
     .from_pretrained("vinai/bertweet-base", num_labels=20)
 
 training_args = TrainingArguments(
     output_dir="test_trainer",
-    evaluation_strategy="epoch"
+    evaluation_strategy="epoch",
+    learning_rate=0.00005,
+    per_device_train_batch_size=64,
+    per_device_eval_batch_size=64,
+    weight_decay=0,
+    num_train_epochs=10
 )
-
-metric = evaluate.load("accuracy")
 
 trainer = Trainer(
     model=model,
     args=training_args,
-    train_dataset=small_train,
-    eval_dataset=small_eval,
-    compute_metrics=compute_metrics
+    train_dataset=train,
+    eval_dataset=eval,
+    compute_metrics=compute_metrics,
 )
 
 trainer.train()
+trainer.save_model("test_trainer")
--- python.py
+++ python.py
@@ -1,14 +1,14 @@
-import datasets
-from transformers import AutoModel, AutoTokenizer
-from fairseq.models.roberta import RobertaModel
-import pandas as pd
+# import datasets
+# from transformers import AutoModel, AutoTokenizer
+# from fairseq.models.roberta import RobertaModel
+# import pandas as pd
 
-bertweet = AutoModel.from_pretrained("vinai/bertweet-base")
+# bertweet = AutoModel.from_pretrained("vinai/bertweet-base")
 
-df = datasets.load_dataset("tweet_eval", "emoji")["train"].to_pandas()
-with pd.option_context(
-    'display.max_rows', None,
-    'display.max_columns', None,
-    'display.precision', 3,
-):
-    print(df.iloc[200])
+# df = datasets.load_dataset("tweet_eval", "emoji")["train"].to_pandas()
+# with pd.option_context(
+#     'display.max_rows', None,
+#     'display.max_columns', None,
+#     'display.precision', 3,
+# ):
+#     print(df.iloc[200])
